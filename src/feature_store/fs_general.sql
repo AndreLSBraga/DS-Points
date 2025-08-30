69508322b80a1e5ba73ac28eb12d1f40785f5df9
André Braga <andre-az@hotmail.com>
2025-08-30 19:41:59 -0300

    Feature store

--- src/feature_store/fs_general.sql
+++ src/feature_store/fs_general.sql
@@ -1,32 +1,29 @@
-WITH tb_rfv AS (
+WITH tb_rfv AS(
 
     SELECT 
         idCustomer,
-
-        CAST(min(julianday('{date}') - julianday(dtTransaction))
+        CAST(MIN(julianday('2024-06-04') - julianday(dtTransaction)) 
             AS INTEGER) + 1 AS recenciaDias,
 
-        COUNT(DISTINCT DATE(dtTransaction)) AS frequenciaDias,
-
-        SUM(CASE
-                WHEN pointsTransaction > 0 THEN pointsTransaction
-            END) AS valorPoints
+        COUNT (DISTINCT DATE(dtTransaction)) as frequenciaDias,
+        
+        SUM (CASE
+                WHEN pointsTransaction > 0 THEN pointsTransaction 
+            END) as pointsTotalAcc
 
     FROM transactions
 
-    WHERE dtTransaction < '{date}'
-    AND dtTransaction >= DATE('{date}', '-21 day')
+    WHERE dtTransaction < '2024-06-04'
+    AND dtTransaction >= DATE('2024-06-04', '-21 day')
 
     GROUP BY idCustomer
-),
 
+    ),
 tb_idade AS (
-
     SELECT
-
         t1.idCustomer,
-
-        CAST(MAX(julianday('{date}') - julianday(t2.dtTransaction))
+        
+        CAST(MAX(julianday('2024-06-04') - julianday(t2.dtTransaction)) 
                 AS INTEGER) + 1 AS idadeBaseDias
 
     FROM tb_rfv AS t1
@@ -34,20 +31,18 @@ tb_idade AS (
     LEFT JOIN transactions AS t2
     ON t1.idCustomer = t2.idCustomer
 
-    GROUP BY t2.idCustomer
-
+    GROUP BY t1.idCustomer
 )
 
 SELECT
-     '{date}' AS dtRef,
-     t1.*,
-     t2.idadeBaseDias,
-     t3.flEmail
+    t1.*,
+    t2.idadeBaseDias,
+    t3.flEmail
 
-FROM tb_rfv AS t1
+FROM tb_rfv AS T1
 
 LEFT JOIN tb_idade AS t2
 ON t1.idCustomer = t2.idCustomer
 
-LEFT JOIN customers AS t3
-ON t1.idCustomer = t3.idCustomer
+LEFT JOIN customers as t3
+ON T1.idCustomer = t3.idCustomer
